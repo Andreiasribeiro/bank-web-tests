3772cf2544a16ef6349eed3f55f7c4ac03c42526
Andréia Ribeiro <asribeirodesign@gmail.com>
2026-04-11 11:54:35 -0300

    Added a test to validate login with invalid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,4 +9,14 @@ describe("login", () => {
     //Assert
     cy.contains("h4", "Make a Transfer").should("be.visible");
   });
+  it("Login with invalid credentials should show error message", () => {
+    //Arrange
+    cy.visit("http://localhost:4000");
+    //Act
+    cy.get("#username").click().type("andreia.ribeiro");
+    cy.get("#senha").click().type("7654321"); //incorrect password
+    cy.contains("button", "Sign In").click();
+    //Assert
+    cy.get(".toast").should("have.text", "Erro no login. Tente novamente.");
+  });
 });
