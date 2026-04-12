5be4dd18884443d8f274912b30cb95f7c2725955
Andréia Ribeiro <asribeirodesign@gmail.com>
2026-04-12 07:04:06 -0300

    Added data from fixture and mpdified the .gitignore file

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,11 @@ beforeEach(() => {
 describe("login", () => {
   it("Login with valid credentials should allow access to the system", () => {
     //Act
-    cy.get("#username").click().type("andreia.ribeiro");
-    cy.get("#senha").click().type("1234567");
+    cy.fixture("credentials").then((credentials) => {
+      cy.get("#username").click().type(credentials.valid.username);
+      cy.get("#senha").click().type(credentials.valid.password);
+    });
+
     cy.screenshot("after-entered-valid-data");
     cy.get("#login-section > .btn").click();
     cy.screenshot("after-clicking-login-button");
@@ -17,8 +20,10 @@ describe("login", () => {
   });
   it("Login with invalid credentials should show error message", () => {
     //Act
-    cy.get("#username").click().type("andreia.ribeiro");
-    cy.get("#senha").click().type("7654321"); //incorrect password
+    cy.fixture("credentials").then((credentials) => {
+      cy.get("#username").click().type(credentials.invalid.username);
+      cy.get("#senha").click().type(credentials.invalid.password);
+    });
     cy.contains("button", "Sign In").click();
     //Assert
     cy.get(".toast").should("have.text", "Erro no login. Tente novamente.");
