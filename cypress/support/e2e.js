c48b57ccd621df03d3fb09fb5d9b821957f373f7
Andréia Ribeiro <asribeirodesign@gmail.com>
2026-04-13 08:30:31 -0300

    Added the Mochawesome HTML report

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+// Test reports in HTML
+import "cypress-mochawesome-reporter/register";
